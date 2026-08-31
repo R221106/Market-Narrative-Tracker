165e2ab56bb728707c4b813fe921e71b426964a1
R221106 <126408169+R221106@users.noreply.github.com>
2026-08-31 19:30:29 +0400

    Update app.py to make it less restrictive

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def get_filtered_articles(topic):
     return filter_articles(
         articles,
         topic,
-        minimum_score=3
+        minimum_score= 2
     )
 # ============================================================
 # FRONTEND PAGES
